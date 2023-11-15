2fa238b3df53962e719afc1033cf0f64efb60e56
Subin <sub_1n@yahoo.com>
2023-11-15 11:49:07 +0200

    mongoose model added

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,31 +1,38 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 // @ desc Get all contacts
 // @ route Get /api/contacts
 // @ access public (access to the api) (will be made private after integrating the authentication)
 
-const getContacts = (req, res) => {
-  res.status(201).json({ message: "Get all contacts" });
-};
-const getContact = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(201).json(contacts);
+});
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact with id ${req.params.id}` });
-};
+});
 
-const addContact = (req, res) => {
-  console.log(req.body);
-  const { name, age, address } = req.body;
-  if (!name || !age || !address) {
+const addContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mandatory");
   }
-  res.status(200).json({ message: "Create Contact" });
-};
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(200).json(contact);
+});
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Updated contact with id ${req.params.id}` });
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Deleted contact with id ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 8000;
+
+connectDb();
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes")); //middleware
